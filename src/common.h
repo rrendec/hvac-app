b99894bbb5af1a3ad1438bef5fa613e97ea5f20c
Radu Rendec <radu@rendec.net>
2021-11-13 11:17:55 -0500

    Change message output format to systemd daemon

--- src/common.h
+++ src/common.h
@@ -2,23 +2,32 @@
 #define __HVAC_COMMON_H__
 
 #include <errno.h>
+#include <systemd/sd-daemon.h>
 
-#define prerr(...) do {								\
-	int __errno = errno;							\
-	fprintf(stderr, __VA_ARGS__);						\
-	fflush(stderr);								\
-	errno = __errno;							\
+#define xprintf(...) do {							\
+	printf(__VA_ARGS__);							\
+	fflush(stdout);								\
 } while (0)
+
+#define xfprintf(stream, ...) do {						\
+	FILE *__stream = (stream);						\
+	fprintf(__stream, __VA_ARGS__);						\
+	fflush(__stream);							\
+} while (0)
+
+#define xprerrf(...) xfprintf(stderr, __VA_ARGS__)
 
 #define first_arg(arg, ...) arg
 #define shift_arg(arg, ...) __VA_OPT__(,) __VA_ARGS__
 
 #define xassert(condition, fallback, ...) do {					\
 	if (!(condition)) {							\
-		prerr("Assertion '%s' failed in %s() [%s:%d]"			\
+		int __errno = errno;						\
+		xprerrf(SD_ERR "Assertion '%s' failed in %s() [%s:%d]"		\
 			__VA_OPT__(": " first_arg(__VA_ARGS__)) "\n",		\
 			#condition, __func__, __FILE__, __LINE__		\
 			__VA_OPT__(shift_arg(__VA_ARGS__)));			\
+		errno = __errno;						\
 		fallback;							\
 	}									\
 } while (0)
