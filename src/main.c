7b53253102edd3a695debd7f19a220d293837333
Radu Rendec <radu@rendec.net>
2023-01-28 17:14:13 -0500

    Move JSON helper functions to a separate unit

--- src/main.c
+++ src/main.c
@@ -13,13 +13,12 @@
 #include <math.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/mman.h>
 #include <sys/timerfd.h>
 #include <modbus/modbus.h>
 #include <civetweb.h>
-#include <cjson/cJSON.h>
 
 #include "common.h"
+#include "json.h"
 
 enum furnace_mode {
 	FURNACE_OFF,
@@ -197,112 +196,6 @@ struct ctrl_data cd_inst = {
 };
 pthread_mutex_t rd_mutex = PTHREAD_MUTEX_INITIALIZER;
 /* end of run data */
-
-int _map_find(const char * const *map, int size, const char *key)
-{
-	int i;
-
-	for (i = 0; i < size; i++)
-		if (strcmp(map[i], key) == 0)
-			return i;
-
-	return -1;
-}
-
-#define map_find(map, key) _map_find(map, ARRAY_SIZE(map), key)
-
-const char *json_get_string(cJSON *json, const char *key, const char *_default)
-{
-	cJSON *node = cJSON_GetObjectItem(json, key);
-	const char *value = cJSON_GetStringValue(node);
-
-	return value ? value : _default;
-}
-
-#define json_map_string(json, key, _default, map) ({				\
-	const char *str = json_get_string(json, key, _default);			\
-	str ? map_find(map, str) : -1;						\
-})
-
-double json_get_number(cJSON *json, const char *key,
-		       double min, double max, double _default)
-{
-	cJSON *node = cJSON_GetObjectItem(json, key);
-	double value = cJSON_GetNumberValue(node);
-
-	return isnan(value) ||
-		(!isnan(min) && value < min) ||
-		(!isnan(max) && value > max) ?
-		_default : value;
-}
-
-int json_read(const char *path, cJSON **json, int *errl, int *errc)
-{
-	int fd, ret = 0;
-	off_t len;
-	char *buf;
-
-	fd = open(path, O_RDONLY, 0);
-	if (fd < 0)
-		return errno;
-
-	len = lseek(fd, 0, SEEK_END);
-	if (len == (off_t)-1) {
-		ret = errno;
-		goto out_close;
-	}
-
-	buf = mmap(NULL, len, PROT_READ, MAP_SHARED, fd, 0);
-	if (buf == MAP_FAILED) {
-		ret = errno;
-		goto out_close;
-	}
-
-	*json = cJSON_ParseWithLength(buf, len);
-
-	if (!*json) {
-		const char *err = cJSON_GetErrorPtr();
-		const char *last = err, *p;
-		int line = 1;
-
-		for (p = buf; p < err; p++)
-			if (*p == '\n') {
-				line++;
-				last = p + 1;
-			}
-
-		if (errl)
-			*errl = line;
-		if (errc)
-			*errc = err - last;
-
-		ret = EILSEQ;
-	}
-
-	munmap(buf, len);
-out_close:
-	close(fd);
-
-	return ret;
-}
-
-int json_write(const char *path, cJSON *json)
-{
-	FILE *f = fopen(path, "w");
-	char *str;
-	int rc;
-
-	if (!f)
-		return errno;
-
-	str = cJSON_Print(json);
-	rc = fwrite(str, strlen(str), 1, f) ? 0 : EIO;
-	free(str);
-
-	rc = fclose(f) ? errno : rc;
-
-	return rc;
-}
 
 static inline const char *cfg_get_string(const char *key, const char *_default)
 {
